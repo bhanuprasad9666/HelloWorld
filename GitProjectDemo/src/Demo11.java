d71676b4a4b80ae8f71c3a8ac100a07217306572
prasad3344 <pdurgaprasad@pdurgaprasad-sa.stratapps.com>
2023-11-09 14:04:03 +0530

    file handled

---
+++ GitProjectDemo/src/Demo11.java
@@ -0,0 +1,6 @@
+
+public class Demo11 {
+public static void main(String[] args) {
+	System.out.println("succesfully completed");
+}
+}
--- GitProjectDemo/src/Hello.java
+++ GitProjectDemo/src/Hello.java
@@ -1,6 +1,7 @@
 
 public class Hello {
 public static void main(String[] args) {
-	System.out.println("elllo world");
+	System.out.println("helllo world");
 }
+
 }
